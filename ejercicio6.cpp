c104ef67b74afc69bcc2be681bb596d40ffd1cac
ichoqueru <ichoquer@unjbg.edu.pe>
2025-05-31 00:58:45 -0500

    Agregar ejercicio6: verificar si el número es primo

---
+++ ejercicio6.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int numPrimo(int x);
+
+int main(){
+	int num1;
+	cout<<"Ingrese un numero: "; cin>>num1;	
+	if(numPrimo(num1)){
+		cout<<"Es primo"<<endl;		
+	}
+	else{
+		cout<<"No es primo"<<endl;
+	}	
+	return 0;
+}
+
+int numPrimo(int x){
+	int divi,i;
+	divi=0;
+    for(i=1;i<=x;i++){
+    	if(x%i==0){
+    		divi++;
+    	}
+    }
+    return (divi==2);
+}
