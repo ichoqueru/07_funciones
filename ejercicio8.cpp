50793ce1d0628161bf67bd9f346a02d29d7e3c76
ichoqueru <ichoquer@unjbg.edu.pe>
2025-05-31 12:31:32 -0500

    Actualizar ejercicio8: agrega función para mcd y otra para mcm

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -1,14 +1,17 @@
 #include<iostream>
 using namespace std;
-int MCD_y_MCM(int x, int y, int& mcd, int& mcm);
+int valorMCD(int x, int y, int& mcd);
+int valorMCM(int num1, int num2, int mcd);
 int main(){
     int num1,num2,mcd,mcm;
     cout<<"Ingrese un numero: "; cin>>num1;
     cout<<"Ingrese otro numero: "; cin>>num2;
     
     if(num1>0 && num2>0){
-    MCD_y_MCM(num1, num2, mcd, mcm);
+    valorMCD(num1, num2, mcd);
     cout<<"\nEl valor del MCD es: "<<mcd<<endl;
+
+    mcm= valorMCM(num1, num2, mcd);
     cout<<"\nEl valor del MCM es: "<<mcm<<endl;
     }
     else{
@@ -17,7 +20,7 @@ int main(){
     return 0;
 }
 
-int MCD_y_MCM(int x, int y, int& mcd, int& mcm){
+int valorMCD(int x, int y, int& mcd){
     int aux,resi;
     mcd=x;
 	aux=y;
@@ -26,6 +29,9 @@ int MCD_y_MCM(int x, int y, int& mcd, int& mcm){
 		mcd=aux;
 		aux=resi;
 	}while(resi!=0);
-    mcm=x*y/mcd;
-	return mcd, mcm;
+	return mcd;
+}
+
+int valorMCM(int num1, int num2, int mcd){
+	return ((num1*num2)/mcd);	
 }
