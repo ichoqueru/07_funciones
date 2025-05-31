e975c7a66f4fc1f4f9180698a20ba70473524808
ichoqueru <ichoquer@unjbg.edu.pe>
2025-05-31 01:01:15 -0500

    Actualizar ejercicio6: agrega condición para que sea positivo

--- ejercicio6.cpp
+++ ejercicio6.cpp
@@ -6,6 +6,14 @@ int numPrimo(int x);
 int main(){
 	int num1;
 	cout<<"Ingrese un numero: "; cin>>num1;	
+    if(num1>=1){
+		cout<<"Es un numero positivo"<<endl; 
+	}
+	else{
+		cout<<"No es un numero positivo";
+		return 0;
+	}
+    
 	if(numPrimo(num1)){
 		cout<<"Es primo"<<endl;		
 	}
