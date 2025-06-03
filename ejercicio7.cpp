17044dbdf13ae76c1259578633582887568db0d2
ichoqueru <ichoquer@unjbg.edu.pe>
2025-06-02 20:31:06 -0500

    Actualizar ejercicio7: agrega su variación sin repetición

--- ejercicio7.cpp
+++ ejercicio7.cpp
@@ -3,14 +3,20 @@ using namespace std;
 int factorial(int x);
 int main(){
 	int m,n,r;
-	cout<<"---COMBINATORIA---"<<endl;
 	cout<<"Ingrese un numero: "; cin>>m;
 	cout<<"Ingrese otro numero: "; cin>>n;
 	r=m-n;
 
+    cout<<endl<<"---COMBINATORIA---"<<endl;
+
 	double comb=(factorial(m))/((factorial(n))*(factorial(r)));
 	cout<<"\nLa combinatoria de "<<m<<" sobre "<<n<<" es: "<<comb<<endl;
     
+    cout<<"\n "<<"---VARIACION SIN REPETICION---"<<endl;
+	double vari;
+	
+	vari=(factorial(m))/(factorial(r));
+	cout<<"\nY su variacion sin repeticion es: "<<vari<<endl;
 	return 0;
 }
 
