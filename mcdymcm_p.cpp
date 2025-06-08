b971c4bbfe88ffdb5272f4a85a502acb65e8dc63
ichoqueru <ichoquer@unjbg.edu.pe>
2025-06-07 23:39:52 -0500

    Actualizar mcdymcm_p: agrega función para mostrar los divisores

--- mcdymcm_p.cpp
+++ mcdymcm_p.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int euclides(int a, int b);
 void MCDyMCM(int a,int b, int *mcd, int *mcm);
+void mostrarDivi(int a, int b);
 int main(){
     int cant,i;
 	cout<<"Cuantos pares de numeros deseas ingresar? "; cin>>cant;
 	for(i=1;i<=cant;i++){
     int x, y, rMCD, rMCM;
-	cout<<"\nPar #"<<i<<endl;
+	cout<<endl<<"\nPar #"<<i<<endl;
     cout<<"Ingrese un numero: "; cin>>x;
 	cout<<"Ingrese otro numero: "; cin>>y;
     if(x==0 || y==0){
@@ -20,6 +21,8 @@ int main(){
 	MCDyMCM(x, y, &rMCD, &rMCM);
 	cout<<"MCD: "<<rMCD<<endl;
 	cout<<"MCM: "<<rMCM<<endl;
+
+    mostrarDivi(x, y);	
     }
 	return 0;
 }
@@ -37,4 +40,13 @@ int euclides(int a, int b){
 void MCDyMCM(int a, int b, int *mcd, int *mcm){
 	*mcd=euclides(a,b);	
 	*mcm=(a*b)/(*mcd);
+}
+
+void mostrarDivi(int a, int b){
+	cout<<"Sus divisores son: ";	
+	for (int i=1; i<=min(a, b); i++) {
+        if (a%i==0 && b%i==0) {
+            cout<<i<<" ";            
+        }
+    }
 }
