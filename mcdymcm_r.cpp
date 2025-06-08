40f9a34d8b06c2c6760a20e4b1420de62f87e92b
ichoqueru <ichoquer@unjbg.edu.pe>
2025-06-07 21:13:34 -0500

    Actualizar mcdymcm_r: añadir ingreso de datos diferentes de cero

--- mcdymcm_r.cpp
+++ mcdymcm_r.cpp
@@ -4,12 +4,20 @@ using namespace std;
 void MCDyMCM(int a, int b, int &mcd, int &mcm);
 
 int main(){
-    int x=18, y=24;
+    int x, y;
+    cout<<"Ingrese un numero: "; cin>>x;
+    cout<<"Ingrese otro numero: "; cin>>y;
+    if(x==0 || y==0){
+    	cout<<"\nLos numeros deben ser diferentes de cero";
+    	return 0;
+    }
+    else{
     int rMCD, rMCM;
     MCDyMCM(x, y, rMCD, rMCM);
     cout<<"MCD: "<<rMCD<<endl;
     cout<<"MCM: "<<rMCM<<endl;
     return 0;
+    }
 }
 
 void MCDyMCM(int a, int b, int &mcd, int &mcm){
